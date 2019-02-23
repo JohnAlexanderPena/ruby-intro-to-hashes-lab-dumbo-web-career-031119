57fce9118f6fcf30f53a030149df77d15857421c
John Pena <jpena91@gmail.com>
2019-02-23 19:21:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,17 +16,15 @@ end
 def monopoly_with_second_tier
 	monopoly = {
 	  :railroads => {
-	    :pieces => 4
+	    :pieces => 4,
+	    :names => {},
+	    :rent_in_dollars => {}
 	  }
 	 }
 end
 
 def monopoly_with_third_tier
-	monopoly = {
-	  :railroads => {
-	    {:pieces => 4}
-	  }
-	 }
+
 end
 
 def monopoly_with_fourth_tier
